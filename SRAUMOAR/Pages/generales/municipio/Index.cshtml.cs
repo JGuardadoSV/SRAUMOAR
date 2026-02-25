6624ab5cdfdd00d03f2180e6e7142ed460f5ebb2
Josue Guardado <jguardadosv@gmail.com>
2026-02-25 14:18:35 -0600

    ok rediseño

--- SRAUMOAR/Pages/generales/municipio/Index.cshtml.cs
+++ SRAUMOAR/Pages/generales/municipio/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SRAUMOAR.Entidades.Generales;
 using SRAUMOAR.Modelos;
@@ -21,19 +22,35 @@ namespace SRAUMOAR.Pages.generales.municipio
 
         public IList<Municipio> Municipio { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public int? DistritoFiltradoId { get; set; }
+
         public async Task OnGetAsync(int? id)
         {
-            if (id.HasValue && id > 0)
+            // Set the bound property if 'id' is passed directly via routing or query matching 'id' param
+            if (id.HasValue) 
+            {
+                DistritoFiltradoId = id;
+            }
+
+            // Populate the dropdown list for the filter
+            ViewData["DistritoId"] = new SelectList(_context.Distritos.OrderBy(d => d.NombreDistrito), "DistritoId", "NombreDistrito", DistritoFiltradoId);
+
+            if (DistritoFiltradoId.HasValue && DistritoFiltradoId > 0)
             {
                 Municipio = await _context.Municipios
-                    .Include(m => m.Distrito).Where(x=>x.DistritoId==id).ToListAsync();
+                    .Include(m => m.Distrito)
+                    .Where(x => x.DistritoId == DistritoFiltradoId)
+                    .OrderBy(m => m.NombreMunicipio)
+                    .ToListAsync();
             }
             else
             {
                 Municipio = await _context.Municipios
-                    .Include(m => m.Distrito).ToListAsync();
+                    .Include(m => m.Distrito)
+                    .OrderBy(m => m.NombreMunicipio)
+                    .ToListAsync();
             }
-           
         }
     }
 }
